4a14b5ed973bfa948e4ae7e28fc9304d4d304a7e
Keith Loise <k8t.loise16@gmail.com>
2021-03-08 18:17:06 +1100

    added accordion section

--- app/src/DataObjects/Sections/AccordionSection.php
+++ app/src/DataObjects/Sections/AccordionSection.php
@@ -2,8 +2,51 @@
 
 namespace {
 
+    use SilverStripe\Forms\CheckboxField;
+    use SilverStripe\Forms\FieldList;
+    use SilverStripe\Forms\GridField\GridField;
+    use SilverStripe\Forms\GridField\GridFieldConfig_RecordEditor;
+    use Symbiote\GridFieldExtensions\GridFieldEditableColumns;
+    use UndefinedOffset\SortableGridField\Forms\GridFieldSortableRows;
+
     class AccordionSection extends Section
     {
         private static $singular_name = "Accordion Section";
+
+        private static $has_many = [
+            'AccordionItems' => AccordionItem::class,
+        ];
+
+        public function getSectionCMSFields(FieldList $fields)
+        {
+            $gridConfig = GridFieldConfig_RecordEditor::create(9999);
+            if ($this->AccordionItems()->Count()) {
+                $gridConfig->addComponent(new GridFieldSortableRows('Sort'));
+            }
+            $gridConfig->addComponent(new GridFieldEditableColumns());
+            $gridColumns = $gridConfig->getComponentByType(GridFieldEditableColumns::class);
+            $gridColumns->setDisplayFields([
+                'Archived' => [
+                    'title' => 'Archive',
+                    'callback' => function($record, $column, $grid) {
+                        return CheckboxField::create($column);
+                    }
+                ]
+            ]);
+
+            $gridField = GridField::create(
+                'AccordionItems',
+                'Accordion Items',
+                $this->AccordionItems(),
+                $gridConfig
+            );
+
+            $fields->addFieldToTab('Root.Main', $gridField);
+        }
+
+        public function getVisibleAccordionItems()
+        {
+            return $this->AccordionItems()->filter('Archived', false);
+        }
     }
 }
