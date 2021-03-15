be59c0dff343d31b4a61b57ea8ba60d19e13cc87
Keith Loise <k8t.loise16@gmail.com>
2021-03-15 16:58:25 +1300

    Updated Winners Section

--- app/src/DataObjects/Sections/WinnersSection.php
+++ app/src/DataObjects/Sections/WinnersSection.php
@@ -2,13 +2,57 @@
 
 namespace {
 
+    use SilverStripe\Forms\FieldList;
+    use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+    use SilverStripe\ORM\ArrayList;
+    use SilverStripe\View\ArrayData;
+
     class WinnersSection extends Section
     {
         private static $singular_name = "Winners Section";
 
         private static $db = [
-            'EventYear' => 'Text',
+            'Content' => 'HTMLText',
         ];
 
+        public function getSectionCMSFields(FieldList $fields)
+        {
+            $fields->addFieldToTab('Root.Main', HTMLEditorField::create('Content'));
+        }
+
+        public function getVisibleEventWinners()
+        {
+            $eventYears = EventYear::get()->filter(['Archived' => false, 'ShowInWinnersPage' => true])->sort('ID', 'DESC');
+            $output = new ArrayList();
+            foreach ($eventYears as $year) {
+                $categories = CategoryHistory::get()->filter(['Archived' => false, 'EventYear' => $year->Name]);
+                $categoryArray = new ArrayList();
+                foreach ($categories as $category) {
+                    $finalists = EventFinalist::get()->filter(['Archived' => false, 'EventYearID' => $year->ID, 'EventCategoryID' => $category->ID, 'Result' => 'Winner']);
+                    if (count($finalists)) {
+                        $finalistsArray = new ArrayList();
+                        foreach($finalists as $finalist) {
+                            $finalistsArray[] = new ArrayData(array(
+                                'Name'    => $finalist->Name,
+                                'Image'   => $finalist->WinnerImage,
+                                'Content' => $finalist->WinnerContent,
+                            ));
+                        }
+                        $categoryArray[] = new ArrayData(array(
+                            'Name' => $category->Name,
+                            'Content' => $category->FinalistsContent,
+                            'Winners' => $finalistsArray
+                        ));
+                    }
+                }
+                $childOutput = [
+                    'Year'        => $year->Name,
+                    'YearContent' => $year->WinnersPageExtraContent,
+                    'Categories'  => $categoryArray
+                ];
+                $output[] = new ArrayData(array('Winners' => $childOutput));
+            }
+            return $output;
+        }
     }
 }
