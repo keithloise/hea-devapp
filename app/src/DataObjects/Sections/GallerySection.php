ab18344488114c2ef11b307ae6691532d536dd2a
Keith Loise <k8t.loise16@gmail.com>
2021-03-15 16:57:16 +1300

    Added Event Gallery Section

--- app/src/DataObjects/Sections/GallerySection.php
+++ app/src/DataObjects/Sections/GallerySection.php
@@ -2,8 +2,25 @@
 
 namespace {
 
+    use SilverStripe\Forms\FieldList;
+    use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+
     class GallerySection extends Section
     {
         private static $singular_name = "Gallery Section";
+
+        private static $db = [
+            'Content' => 'HTMLText'
+        ];
+
+        public function getSectionCMSFields(FieldList $fields)
+        {
+            $fields->addFieldToTab('Root.Main', HTMLEditorField::create('Content'));
+        }
+
+        public function getVisibleGallery()
+        {
+
+        }
     }
 }
